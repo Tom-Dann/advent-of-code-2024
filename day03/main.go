40dfe04371e5572d2dffe878520f6805f0745bc0
Tom Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-12-03 08:35:01 +0000

    Remove loop

--- day03/main.go
+++ day03/main.go
@@ -15,21 +15,19 @@ func multMatch(match []string) int {
 
 func solve(lines []string) {
 	part1, part2, enabled := 0, 0, true
-	re := regexp.MustCompile(`(?:mul\((\d{1,3}),(\d{1,3})\))|(?:do\(\))|(?:don't\(\))`)
-	for _, line := range lines {
-		matches := re.FindAllStringSubmatch(line, -1)
-		for _, match := range matches {
-			switch match[0][2] {
-			case '(': // do()
-				enabled = true
-			case 'n': // don't()
-				enabled = false
-			case 'l': // mul(a,b)
-				x := multMatch(match)
-				part1 += x
-				if enabled {
-					part2 += x
-				}
+	re := regexp.MustCompile(`mul\((\d{1,3}),(\d{1,3})\)|do(?:n't)?\(\)`)
+	matches := re.FindAllStringSubmatch(lines[0], -1)
+	for _, match := range matches {
+		switch match[0][2] {
+		case '(': // do()
+			enabled = true
+		case 'n': // don't()
+			enabled = false
+		case 'l': // mul(a,b)
+			x := multMatch(match)
+			part1 += x
+			if enabled {
+				part2 += x
 			}
 		}
 	}
@@ -38,6 +36,6 @@ func solve(lines []string) {
 }
 
 func main() {
-	input := utils.ReadInput("input.txt", "\n")
+	input := utils.ReadInput("input.txt", "\n\n")
 	utils.TimeFunctionInput(solve, input)
 }
