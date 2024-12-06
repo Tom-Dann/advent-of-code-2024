601b82ee715f93a2a55e5bd090ffaca400770285
Tom Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-12-06 10:34:58 +0000

    Combine both parts: 1s -> 250ms

--- day06/main.go
+++ day06/main.go
@@ -23,23 +23,6 @@ func parseGrid(input []string) (map[Position]bool, Position) {
 	return grid, pos
 }
 
-func part1(input []string) {
-	grid, pos := parseGrid(input)
-	inGrid, dir := true, Position{0, -1}
-	seen := map[Position]struct{}{}
-	for inGrid {
-		seen[pos] = struct{}{}
-		newPos, wall := Position{pos.x + dir.x, pos.y + dir.y}, false
-		wall, inGrid = grid[newPos]
-		if wall {
-			dir = Position{-dir.y, dir.x}
-		} else {
-			pos = newPos
-		}
-	}
-	fmt.Println("Part 1:", len(seen))
-}
-
 func doesItLoop(grid map[Position]bool, added Position, start Position, results chan<- int) {
 	type key struct{ p, d Position }
 	dir, pos := Position{0, -1}, start
@@ -47,12 +30,12 @@ func doesItLoop(grid map[Position]bool, added Position, start Position, results 
 	for {
 		seen[key{pos, dir}] = struct{}{}
 		newPos := Position{pos.x + dir.x, pos.y + dir.y}
-		wall, valid := grid[newPos]
-		if !valid {
+		wall, inGrid := grid[newPos]
+		if !inGrid {
 			results <- 0
 			return
 		}
-		if wall || (newPos.x == added.x && newPos.y == added.y) {
+		if wall || newPos == added {
 			dir = Position{-dir.y, dir.x}
 		} else {
 			pos = newPos
@@ -65,18 +48,31 @@ func doesItLoop(grid map[Position]bool, added Position, start Position, results 
 	}
 }
 
-func part2(input []string) {
-	grid, pos := parseGrid(input)
-	count, n := 0, len(grid)
-	results := make(chan int, n)
-	for try, wall := range grid {
-		if !(wall || (try.x == pos.x && try.y == pos.y)) {
-			go doesItLoop(grid, try, pos, results)
+func solve(input []string) {
+	grid, start := parseGrid(input)
+	pos, inGrid, dir := start, true, Position{0, -1}
+	seen := map[Position]struct{}{}
+	for inGrid {
+		seen[pos] = struct{}{}
+		newPos, wall := Position{pos.x + dir.x, pos.y + dir.y}, false
+		wall, inGrid = grid[newPos]
+		if wall {
+			dir = Position{-dir.y, dir.x}
+		} else {
+			pos = newPos
+		}
+	}
+	fmt.Println("Part 1:", len(seen))
+
+	count, results := 0, make(chan int, len(seen))
+	for try := range seen {
+		if try != start {
+			go doesItLoop(grid, try, start, results)
 		} else {
 			results <- 0
 		}
 	}
-	for i := 0; i < n; i++ {
+	for i := 0; i < len(seen); i++ {
 		count += <-results
 	}
 	fmt.Println("Part 2:", count)
@@ -84,6 +80,5 @@ func part2(input []string) {
 
 func main() {
 	input := utils.ReadInput("input.txt", "\n")
-	utils.TimeFunctionInput(part1, input)
-	utils.TimeFunctionInput(part2, input)
+	utils.TimeFunctionInput(solve, input)
 }
