0723bb1f28418f42c798f91c4e86faa171194752
Tom Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-12-11 22:47:28 +0000

    Tidy

--- day11/main.go
+++ day11/main.go
@@ -6,12 +6,15 @@ import (
 	"utils"
 )
 
+func parseInt(s string) int64 {
+	n, _ := strconv.ParseInt(s, 10, 64)
+	return n
+}
+
 func solve(input []string) {
-	counts := map[int64]int64{}
-	totals := [2]int64{}
+	counts, totals := map[int64]int64{}, [2]int64{}
 	for _, s := range input {
-		n, _ := strconv.ParseInt(s, 10, 64)
-		counts[n]++
+		counts[parseInt(s)]++
 	}
 	for i := 1; i <= 75; i++ {
 		newCount := map[int64]int64{}
@@ -19,10 +22,8 @@ func solve(input []string) {
 			if k == 0 {
 				newCount[1] += v
 			} else if s := strconv.FormatInt(k, 10); len(s)%2 == 0 {
-				a, _ := strconv.ParseInt(s[:len(s)/2], 10, 64)
-				b, _ := strconv.ParseInt(s[len(s)/2:], 10, 64)
-				newCount[a] += v
-				newCount[b] += v
+				newCount[parseInt(s[:len(s)/2])] += v
+				newCount[parseInt(s[len(s)/2:])] += v
 			} else {
 				newCount[k*2024] += v
 			}
@@ -30,7 +31,7 @@ func solve(input []string) {
 		counts = newCount
 		if i == 25 || i == 75 {
 			for _, v := range counts {
-				totals[(i-25)/50] += v
+				totals[i/75] += v
 			}
 		}
 	}
