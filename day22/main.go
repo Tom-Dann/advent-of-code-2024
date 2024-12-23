3bf9c0ecc3cb5c174bd3025efe676a74780d0b8d
Tom Dann <110121463+Tom-Dann@users.noreply.github.com>
2024-12-23 10:47:06 +0000

    Tidy loops

--- day22/main.go
+++ day22/main.go
@@ -7,9 +7,9 @@ import (
 )
 
 func nextSecret(n int) int {
-	n = (n ^ (n << 6)) % 16777216
-	n = (n ^ (n >> 5)) % 16777216
-	n = (n ^ (n << 11)) % 16777216
+	n = (n ^ (n << 6)) % (1 << 24)
+	n = (n ^ (n >> 5)) % (1 << 24)
+	n = (n ^ (n << 11)) % (1 << 24)
 	return n
 }
 
@@ -18,25 +18,19 @@ func solve(lines []string) {
 	bananas := map[[4]int]int{}
 	for _, s := range lines {
 		secret, _ := strconv.Atoi(s)
-		prices := [2001]int{secret % 10}
+		seen, key := map[[4]int]bool{}, [4]int{0, 0, 0, 0}
+		prev := secret % 10
 		for i := 1; i <= 2000; i++ {
 			secret = nextSecret(secret)
-			prices[i] = secret % 10
-			part1 += secret
-		}
-		seen := map[[4]int]bool{}
-		for i := 4; i < 2001; i++ {
-			key := [4]int{
-				prices[i-3] - prices[i-4],
-				prices[i-2] - prices[i-3],
-				prices[i-1] - prices[i-2],
-				prices[i] - prices[i-1],
-			}
-			if !seen[key] {
-				bananas[key] += prices[i]
+			curr := secret % 10
+			key = [4]int{key[1], key[2], key[3], curr - prev}
+			if i >= 4 && !seen[key] {
+				bananas[key] += curr
 				seen[key] = true
 			}
+			prev = curr
 		}
+		part1 += secret
 	}
 	for _, count := range bananas {
 		if count > part2 {
